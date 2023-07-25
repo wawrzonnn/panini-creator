201bc8655ee1eca81f5a6735a672becebc82e136
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-07-25 02:31:17 +0200

    remove form and success screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,12 @@ import "./App.scss";
 import React, { useState } from 'react';
 import { AnimatePresence } from 'framer-motion';
 import WelcomeScreen from './views/WelcomeScreen/WelcomeScreen';
-import PaniniCreator from './views/PaniniCreator/PaniniCreator';
-import SuccessScreen from './views/SuccessScreen/SuccessScreen';
+
 
 const App: React.FC = () => {
-  const [currentScreen, setCurrentScreen] = useState<'welcome' | 'panini' | 'success'>('welcome');
-
-  const handleStart = () => {
-    setCurrentScreen('panini');
-  };
-
-  const handleOrder = () => {
-    setCurrentScreen('success');
-  };
 
   return (
-    <AnimatePresence mode="wait">
-      {currentScreen === 'welcome' && <WelcomeScreen onStart={handleStart} />}
-      {currentScreen === 'panini' && <PaniniCreator onOrder={handleOrder} />}
-      {currentScreen === 'success' && <SuccessScreen />}
-    </AnimatePresence>
+    <WelcomeScreen />
   );
 };
 
